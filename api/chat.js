67216451adcfb5515dbfa44ebccb5c4b770b75c6
anatoliacraftsart-1 <anatoliacrafts.art@gmail.com>
2026-02-14 02:02:19 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
 
   try {
     // Listendeki en stabil ve yeni model: gemini-2.5-flash
-    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`;
+   const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=${apiKey}`;
 
     const response = await fetch(url, {
       method: 'POST',
